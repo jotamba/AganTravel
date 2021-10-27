f6bc3fdecddbc8392eb06b1d7af792309a44e3f7
ASUS <tfadriangawa@gmail.com>
2021-10-27 21:01:47 +0700

    aktivasi email selesai

---
+++ process/confirmEmail.php
@@ -0,0 +1,25 @@
+<?php
+    include('db.php');
+
+    if(isset($_GET['code'])){
+        $code = $_GET['code'];
+        $sql = "SELECT * FROM users where verif_code = '$code'";
+        $query = mysqli_query($con,$sql);
+        if(mysqli_num_rows($query) > 0){
+            $user = mysqli_fetch_assoc($query);
+            $id = $user['id'];
+            $sql =  "UPDATE users set isVerified=1 where id=$id";
+            $query = mysqli_query($con,$sql);
+            if($query){
+                echo "VERIFIKASI BERHASIL ";
+            }else{
+                echo "VERIFIKASI GAGAL ERROR : ".$query;
+            }
+        }else {
+            echo "CODE TIDAK DITEMUKAN ATAU TIDAK VALID";
+        }
+    }else {
+        echo "code ga nih";
+    }
+
+?>
